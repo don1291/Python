138059a469e2bc647069f39512fadbb75f5eb08b
don1291 <32994255+don1291@users.noreply.github.com>
2021-02-18 21:25:45 -0500

    Create ingredient.py

---
+++ ingredient.py
@@ -0,0 +1,21 @@
+#Ingredient Adjuster
+#9/15/2016
+
+cookies = float (input("How many cookies are being made?  "))
+
+#Amount of sugar
+sugar = 1.5
+#Amount of butter
+butter = 1
+#Amount of flour
+flour = 2.75
+#Calculate the total sugar
+total_sugar = (cookies * sugar)/48
+#Calculate the total butter
+total_butter = (cookies * butter)/48
+#Calculate the total flour
+total_flour = (cookies * flour)/48
+#The results 
+print(total_sugar)
+print(total_butter)
+print(total_flour
