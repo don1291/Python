c329ccdddc3861ad60ca8d2241c81ab3a2b5b23d
don1291 <32994255+don1291@users.noreply.github.com>
2021-02-18 21:20:55 -0500

    Create Personal Info.py

---
+++ Personal Info.py
@@ -0,0 +1,9 @@
+#Personal Information
+name = input("what is your name?")
+print("John Smith")
+address =("What is your address?")
+print("123 Maple Street, Beverly Hills, California, 90102")
+phone = input("What is your phone number?")
+print("122-456-7890")
+major = input("What is your major?")
+print("Infomation Technology")
