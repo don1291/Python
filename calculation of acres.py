55922f3ffbe40a97a6b9435a63140e1bdd5aa593
don1291 <32994255+don1291@users.noreply.github.com>
2021-02-18 21:23:14 -0500

    Create calculation of acres.py

---
+++ calculation of acres.py
@@ -0,0 +1,7 @@
+Ex.3 In this program, we were asked to enter the total square feet and calculate the numbers of acres.
+This the result after excute:
+sqft = input("Enter the square feet tract in land:")
+land_size = int(sqft) / 43560
+print (land_size)
+Enter the square feet tract in land:100000
+2.295684113865932
